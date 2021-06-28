33f10d2e69bb6ff1c5b01feb02029bc437abb31a
Barox <75582570+baroxFPTU@users.noreply.github.com>
2021-06-28 21:31:13 +0700

    add animation with scrollRevealJs

---
+++ animation.js
@@ -0,0 +1,23 @@
+        // home-left, home-right .card, .home-right
+        //  .widget, divTable, section-heading, learn-left, learn-right, service-left, service-right, fanstatic p,action-content
+        window.sr = ScrollReveal({reset: true});
+        sr.reveal('.header', {distance: '100px', origin:'top', delay:200, duration: 800});
+        sr.reveal('.home-left', {distance: '100px', origin:'left', delay:300, duration: 800, opacity: 0});
+        sr.reveal('.home-right .card', {distance: '500px', origin:'right', delay:300, duration: 800, opacity: 0});
+        sr.reveal('.home-right .widget', {distance: '500px', origin:'right', delay:400, duration: 800, opacity: 0});
+        sr.reveal('.market .section-heading', {distance: '100px', origin:'left', delay:300, duration:800});
+        sr.reveal('.divTable', {distance: '100px', origin:'bottom', delay:300, duration:800});
+        sr.reveal('.learn-left', {distance: '200px', origin:'left', delay:300, duration:800});
+        sr.reveal('.learn-right', {distance: '200px', origin:'right', delay:400, duration:800});
+        sr.reveal('.service-left', {distance: '200px', origin:'left', delay:300, duration:800});
+        sr.reveal('.service-content', { scale:0.8, delay:350, duration:800, interval: 200});
+        sr.reveal('.fanstatic', {distance: '500px', origin:'right', delay:300, duration:1000});
+        sr.reveal('.action',{ scale:0.5, delay:300, duration:800,delay:300,  interval: 200, duration: 1000});
+
+
+
+        
+
+
+
+
